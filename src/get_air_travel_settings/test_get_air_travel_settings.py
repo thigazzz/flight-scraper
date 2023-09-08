877f19448c5fe2780d82e60ce0ec61dceba7ef21
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-09-08 11:59:19 -0300

    fix: fix tests

--- src/get_air_travel_settings/test_get_air_travel_settings.py
+++ src/get_air_travel_settings/test_get_air_travel_settings.py
@@ -2,7 +2,7 @@ import pytest
 from get_air_travel_settings import get_air_travel_settings, validate_input_date
 
 
-@pytest.mark.input_user
+@pytest.mark.user_input
 def test_get_settings_from_user_input(monkeypatch):
     mock_inputs = iter(["São Paulo", "Rio de Janeiro", "07 08 2023", "14 08 2023"])
     monkeypatch.setattr("builtins.input", lambda _: next(mock_inputs))
@@ -12,17 +12,17 @@ def test_get_settings_from_user_input(monkeypatch):
     assert from_where == "São Paulo"
     assert to_where == "Rio de Janeiro"
     assert departure_date == {"dia": "7", "mes": "agosto", "ano": "2023"}
-    print(departure_date)
     assert return_date == {"dia": "14", "mes": "agosto", "ano": "2023"}
 
 
-@pytest.mark.input_user
+@pytest.mark.user_input
 def test_validate_if_input_date_is_in_correct_format():
     validaded_input_date = validate_input_date("7 08 2023")
 
     assert validaded_input_date == {"dia": "7", "mes": "agosto", "ano": "2023"}
 
 
+@pytest.mark.user_input
 @pytest.mark.skip(reason="Não tenho conhecimento ainda para fazer tal teste")
 def test_input_validation(monkeypatch, capsys):
     mock_inputs = iter(["1", "Rio de Janeiro", "07 08 2023", "14 08 2023"])
