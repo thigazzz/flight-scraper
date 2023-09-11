b54910f8f01423d7d3d1893a0b4a5fa404a6242c
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-09-11 17:29:15 -0300

    refactor: split main insert air travel settings function in minors

--- src/extract_flight_informations/test_extract_flight_informations.py
+++ src/extract_flight_informations/test_extract_flight_informations.py
@@ -3,9 +3,25 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from datetime import datetime
+from typing import List
+
+
+# utils
+def is_chosen_month_is_previous_first_shown_on_screen(
+    choosen_month: int, first_month_on_screen: int
+) -> bool:
+    if choosen_month < first_month_on_screen:
+        return True
+    return False
+
+
+def is_month_chosen_is_posteior_to_second_shown_on_screen(
+    choosen_month: int, second_month_on_screen: int
+) -> bool:
+    if choosen_month > second_month_on_screen:
+        return True
+    return False
 
 
 class WebRobot:
@@ -14,24 +30,6 @@ class WebRobot:
 
     def access_skyscanner_site(self):
         self.web_driver.get("https://www.kayak.com.br/")
-        # try:
-        #     if self.web_driver.find_element(By.XPATH, "//*[text()='Are you a person or a robot?']"):
-        #         time.sleep(3)
-        #         self.__break_captcha()
-        # except:
-        #     pass
-
-    def __break_captcha(self):
-        self.web_driver.find_element(
-            By.XPATH, "//*[text()='Are you a person or a robot?']"
-        ).click()
-        actions = ActionChains(self.web_driver)
-        actions.send_keys(Keys.TAB)
-        actions.key_down(Keys.ENTER)
-        actions.perform()
-        time.sleep(10)
-        actions.key_up(Keys.ENTER)
-        actions.perform()
 
     def insert_air_travel_settings(self, air_travel_settings: list):
         from_where, to_where, departure_date, return_date = air_travel_settings
@@ -47,56 +45,17 @@ class WebRobot:
             By.CSS_SELECTOR,
             "span[aria-label='Seleção da data de início no calendário']",
         ).click()
-
-        while True:
-            displayed_months = self.web_driver.find_elements(
-                By.CLASS_NAME, "onx_-double"
-            )
-            months = []
-            for displayed_month in displayed_months:
-                year_and_month = displayed_month.get_attribute("data-month")
-                year_and_month = year_and_month.split("-")
-
-                months.append(int(f"{year_and_month[0]}{year_and_month[1]}"))
-
-            init_month = int(
-                datetime.strptime(
-                    f"{departure_date['ano']}{departure_date['mes']}", "%Y%B"
-                )
-                .date()
-                .strftime("%Y%m")
-            )
-
-            if init_month < months[0]:
-                print(
-                    "mes escolhido para inicio é menor e nao esta sendo mostrado na tela"
-                )
-                self.web_driver.find_element(
-                    By.CSS_SELECTOR, 'button[aria-label="Mês anterior"]'
-                ).click()
-            elif init_month > months[1]:
-                print(
-                    "mes escolhido para inicio  é maior e nao esta sendo mostrado na tela"
-                )
-                self.web_driver.find_element(
-                    By.CSS_SELECTOR, 'button[aria-label="Próximo mês"]'
-                ).click()
-            else:
-                print(
-                    f"div[aria-label*='{departure_date['dia']} de {departure_date['mes']} de {departure_date['ano']}']"
-                )
-                self.web_driver.find_element(
-                    By.CSS_SELECTOR,
-                    f"div[aria-label*='{departure_date['dia']} de {departure_date['mes']} de {departure_date['ano']}']",
-                ).click()
-                print("mes esta sendo mostrado na tela")
-                break
+        self.select_choosen_month(departure_date)
 
         self.web_driver.find_element(
             By.CSS_SELECTOR,
             "span[aria-label='Seleção da data de término no calendário']",
         ).click()
+        self.select_choosen_month(return_date)
+
+    def select_choosen_month(self, date: dict):
         while True:
+            # pego meses na tela e formato
             displayed_months = self.web_driver.find_elements(
                 By.CLASS_NAME, "onx_-double"
             )
@@ -108,35 +67,44 @@ class WebRobot:
                 months.append(int(f"{year_and_month[0]}{year_and_month[1]}"))
 
             init_month = int(
-                datetime.strptime(f"{return_date['ano']}{return_date['mes']}", "%Y%B")
+                datetime.strptime(f"{date['ano']}{date['mes']}", "%Y%B")
                 .date()
                 .strftime("%Y%m")
             )
-
-            if init_month < months[0]:
+            if self.is_choosen_month_displayed_on_screen(init_month, months):
                 print(
-                    "mes escolhido para inicio é menor e nao esta sendo mostrado na tela"
-                )
-                self.web_driver.find_element(
-                    By.CSS_SELECTOR, 'button[aria-label="Mês anterior"]'
-                ).click()
-            elif init_month > months[1]:
-                print(
-                    "mes escolhido para inicio  é maior e nao esta sendo mostrado na tela"
-                )
-                self.web_driver.find_element(
-                    By.CSS_SELECTOR, 'button[aria-label="Próximo mês"]'
-                ).click()
-            else:
-                print(
-                    f"div[aria-label*='{return_date['dia']} de {return_date['mes']} de {return_date['ano']}']"
+                    f"div[aria-label*='{date['dia']} de {date['mes']} de {date['ano']}']"
                 )
                 self.web_driver.find_element(
                     By.CSS_SELECTOR,
-                    f"div[aria-label*='{return_date['dia']} de {return_date['mes']} de {return_date['ano']}']",
+                    f"div[aria-label*='{date['dia']} de {date['mes']} de {date['ano']}']",
                 ).click()
-                print("mes esta sendo mostrado na tela")
                 break
+
+    def is_choosen_month_displayed_on_screen(
+        self, choosen_month: int, months_displayed_on_screen: List[int]
+    ) -> bool:
+        if is_chosen_month_is_previous_first_shown_on_screen(
+            choosen_month, months_displayed_on_screen[0]
+        ):
+            self.click_prev_month()
+            return False
+        elif is_month_chosen_is_posteior_to_second_shown_on_screen(
+            choosen_month, months_displayed_on_screen[1]
+        ):
+            self.click_pos_month()
+            return False
+        return True
+
+    def click_prev_month(self):
+        self.web_driver.find_element(
+            By.CSS_SELECTOR, 'button[aria-label="Mês anterior"]'
+        ).click()
+
+    def click_pos_month(self):
+        self.web_driver.find_element(
+            By.CSS_SELECTOR, 'button[aria-label="Próximo mês"]'
+        ).click()
 
 
 @pytest.fixture
@@ -189,6 +157,3 @@ def test_insert_user_air_travel_settings_in_search_inputs(web_robot: WebRobot):
             'span[aria-label="Seleção da data de término no calendário"] span:nth-child(2)',
         ).text
     )
-
-
-# aria-label="quinta, 7 de setembro de 2023. preço médio. Selecionar como data de ida"
