e460861a514a6f0e9f7db5dd24214b7e67ca8c4d
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-09-19 11:49:20 -0300

    refactor: excel robot class

--- src/save_informations_in_excel/test_save_informations_in_excel.py
+++ src/save_informations_in_excel/test_save_informations_in_excel.py
@@ -6,24 +6,44 @@ import time
 
 
 class ExcelRobot:
-    def __init__(self) -> None:
-        self.open_excel_blank_file()
+    excel_file_path = os.path.abspath("data/air-travels.xlsx")
 
-    def open_excel_blank_file(self):
-        if os.path.isfile(os.path.abspath("data/air-travels.xlsx")) == False:
-            worksheet = Workbook()
-            worksheet.save(os.path.abspath("data/air-travels.xlsx"))
+    def __init__(self) -> None:
+        self.create_blank_excel_file()
+        self.excel = self.get_excel()
+
+    def create_blank_excel_file(self):
+        if self.is_file_excel_exists() == False:
+            Workbook().save(self.excel_file_path)
+
+    def is_file_excel_exists(self) -> bool:
+        return os.path.isfile(self.excel_file_path)
+
+    def get_excel(self):
+        return load_workbook(self.excel_file_path)
+
+    def get_actual_sheet(self):
+        return self.excel.active
 
     def insert_informations_in_excel(self, informations: Dict):
-        excel_file = load_workbook(os.path.abspath("data/air-travels.xlsx"))
-        sheet = excel_file.active
         index = 0
-        for row in sheet.iter_rows(min_row=2, min_col=1, max_row=6, max_col=3):
-            row[0].value = informations[index]["price"]
-            row[1].value = informations[index]["departure time"]
-            row[2].value = informations[index]["disembarkation time"]
+        for row in self.iterate_rows_of_length_informations(informations):
+            self.insert_in_cell(row[0], informations[index]["price"])
+            self.insert_in_cell(row[1], informations[index]["departure time"])
+            self.insert_in_cell(row[2], informations[index]["disembarkation time"])
             index += 1
-        excel_file.save("data/air-travels.xlsx")
+
+    def iterate_rows_of_length_informations(self, data):
+        sheet = self.get_actual_sheet()
+        return sheet.iter_rows(
+            min_row=2, min_col=1, max_row=len(data) + 1, max_col=len(data[0])
+        )
+
+    def insert_in_cell(self, row, data):
+        row.value = data
+
+    def save_excel(self):
+        self.excel.save(self.excel_file_path)
 
 
 @pytest.fixture
@@ -65,6 +85,8 @@ def test_insert_extracted_informations_in_excel_file(excel_open_and_delete):
 
     excel_robot = ExcelRobot()
     excel_robot.insert_informations_in_excel(mock_extracted_informations)
+    excel_robot.save_excel()
+
     excel_file = load_workbook(os.path.abspath("data/air-travels.xlsx"))
     sheet = excel_file.active
 
